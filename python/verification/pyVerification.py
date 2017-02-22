63873854b799514996a59f7930918c0274655f7e
zhonghp <zhonghp@foxmail.com>
2017-02-22 16:38:00 +0800

    update pyVerification code.

--- python/verification/pyVerification.py
+++ python/verification/pyVerification.py
@@ -3,7 +3,7 @@
 # @Author: vasezhong
 # @Date:   2017-02-22 14:56:11
 # @Last Modified by:   vasezhong
-# @Last Modified time: 2017-02-22 15:09:16
+# @Last Modified time: 2017-02-22 16:37:40
 
 import caffe
 import numpy as np
@@ -16,7 +16,7 @@ class VerificationLayer(caffe.Layer):
     def reshape(self, bottom, top):
         if bottom[0].count != bottom[1].count:
             raise Exception("Inputs must have the same dimensions.")
-        top[0].reshape(bottom[0].shape)
+        top[0].reshape(*bottom[0].data.shape)
 
     def forward(self, bottom, top):
         top[0].data[...] = bottom[0].data == bottom[1].data
