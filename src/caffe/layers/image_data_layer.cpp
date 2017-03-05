42039c39fe98ddd5607d1072538d19463d93646a
zhonghp <zhonghp@foxmail.com>
2017-03-05 15:00:53 +0800

    add patch cropping code in image_data_layer.

--- src/caffe/layers/image_data_layer.cpp
+++ src/caffe/layers/image_data_layer.cpp
@@ -1,11 +1,13 @@
 #ifdef USE_OPENCV
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include <fstream>  // NOLINT(readability/streams)
 #include <iostream>  // NOLINT(readability/streams)
 #include <string>
 #include <utility>
 #include <vector>
+#include <algorithm>
 
 #include "caffe/data_transformer.hpp"
 #include "caffe/layers/base_data_layer.hpp"
@@ -15,11 +17,28 @@
 #include "caffe/util/math_functions.hpp"
 #include "caffe/util/rng.hpp"
 
+using namespace std;
+
 namespace caffe {
 
 template <typename Dtype>
 ImageDataLayer<Dtype>::~ImageDataLayer<Dtype>() {
   this->StopInternalThread();
+}
+
+int get_patch_size(const int& left_idx, const int& top_idx,
+    const int& right_idx, const int& bottom_idx,
+    const int& img_width, const int& img_height) {
+
+  CHECK((left_idx >= 0 && left_idx < img_width)
+      && (right_idx >= 0 && right_idx < img_width)
+      && (top_idx >= 0 && top_idx < img_height)
+      && (bottom_idx >= 0 && bottom_idx < img_height)) << "Crop must be in the origin image.";
+
+  int max_val = max(right_idx - left_idx, bottom_idx - top_idx);
+  max_val = min(max_val, img_width - left_idx);
+  max_val = min(max_val, img_height - top_idx);
+  return max_val;
 }
 
 template <typename Dtype>
@@ -29,6 +48,11 @@ void ImageDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
   const int new_width  = this->layer_param_.image_data_param().new_width();
   const bool is_color  = this->layer_param_.image_data_param().is_color();
   string root_folder = this->layer_param_.image_data_param().root_folder();
+
+  const int left_idx = this->layer_param_.image_data_param().left_idx();
+  const int top_idx = this->layer_param_.image_data_param().top_idx();
+  const int right_idx = this->layer_param_.image_data_param().right_idx();
+  const int bottom_idx = this->layer_param_.image_data_param().bottom_idx();
 
   CHECK((new_height == 0 && new_width == 0) ||
       (new_height > 0 && new_width > 0)) << "Current implementation requires "
@@ -72,9 +96,26 @@ void ImageDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
     lines_id_ = skip;
   }
   // Read an image, and use it to initialize the top blob.
-  cv::Mat cv_img = ReadImageToCVMat(root_folder + lines_[lines_id_].first,
-                                    new_height, new_width, is_color);
-  CHECK(cv_img.data) << "Could not load " << lines_[lines_id_].first;
+//  cv::Mat cv_img = ReadImageToCVMat(root_folder + lines_[lines_id_].first,
+//                                    new_height, new_width, is_color);
+
+  cv::Mat cv_img_origin = ReadImageToCVMat(root_folder + lines_[lines_id_].first, is_color);
+  CHECK(cv_img_origin.data) << "Could not load " << lines_[lines_id_].first;
+
+  if (left_idx >= 0 && top_idx >= 0 && right_idx >= 0 && bottom_idx >= 0) {
+    const int img_width = cv_img_origin.cols;
+    const int img_height = cv_img_origin.rows;
+    const int patch_size = get_patch_size(left_idx, top_idx, right_idx, bottom_idx, img_width, img_height);
+    cv::Rect roi(left_idx, top_idx, patch_size, patch_size);
+    cv_img_origin = cv_img_origin(roi);
+
+    LOG(INFO) << "DataLayerSetUp Patch size: " << patch_size;
+  }
+
+  cv::Mat cv_img;
+  cv::resize(cv_img_origin, cv_img, cv::Size(new_width, new_height));
+  CHECK(cv_img.data) << "Could not get patch from " << lines_[lines_id_].first;
+
   // Use data_transformer to infer the expected blob shape from a cv_image.
   vector<int> top_shape = this->data_transformer_->InferBlobShape(cv_img);
   this->transformed_data_.Reshape(top_shape);
@@ -99,7 +140,7 @@ void ImageDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
 }
 
 int random_data(int i) {
-	return caffe_rng_rand() % i;
+  return caffe_rng_rand() % i;
 }
 
 template <typename Dtype>
@@ -110,23 +151,23 @@ void ImageDataLayer<Dtype>::ShuffleImages() {
   shuffle(lines_.begin(), lines_.end(), prefetch_rng);
   */
   LOG(INFO) << "Shuffling the images.";
-	const int lines_size = lines_.size();
+  const int lines_size = lines_.size();
 
-	ImageDataParameter image_data_param = this->layer_param_.image_data_param();
-	const int pair_size = image_data_param.pair_size();
-	const int pair_num = lines_size / pair_size;
+  ImageDataParameter image_data_param = this->layer_param_.image_data_param();
+  const int pair_size = image_data_param.pair_size();
+  const int pair_num = lines_size / pair_size;
 
-	vector<int> pair_idxs;
-	for (int i = 0; i < pair_num; i++)
-		pair_idxs.push_back(i);
+  vector<int> pair_idxs;
+  for (int i = 0; i < pair_num; i++)
+    pair_idxs.push_back(i);
 
-	vector<std::pair<std::string, int> > tmp_lines;
-	std::random_shuffle(pair_idxs.begin(), pair_idxs.end(), random_data);
-	for (int i = 0; i < pair_num; i++)
-		for (int j = 0; j < pair_size; j++)
-			tmp_lines.push_back(lines_[pair_idxs[i] * pair_size + j]);
+  vector<std::pair<std::string, int> > tmp_lines;
+  std::random_shuffle(pair_idxs.begin(), pair_idxs.end(), random_data);
+  for (int i = 0; i < pair_num; i++)
+    for (int j = 0; j < pair_size; j++)
+      tmp_lines.push_back(lines_[pair_idxs[i] * pair_size + j]);
 
-	lines_ = tmp_lines;
+  lines_ = tmp_lines;
 }
 
 // This function is called on prefetch thread
@@ -146,11 +187,32 @@ void ImageDataLayer<Dtype>::load_batch(Batch<Dtype>* batch) {
   const bool is_color = image_data_param.is_color();
   string root_folder = image_data_param.root_folder();
 
+  const int left_idx = this->layer_param_.image_data_param().left_idx();
+  const int top_idx = this->layer_param_.image_data_param().top_idx();
+  const int right_idx = this->layer_param_.image_data_param().right_idx();
+  const int bottom_idx = this->layer_param_.image_data_param().bottom_idx();
+
   // Reshape according to the first image of each batch
   // on single input batches allows for inputs of varying dimension.
-  cv::Mat cv_img = ReadImageToCVMat(root_folder + lines_[lines_id_].first,
-      new_height, new_width, is_color);
-  CHECK(cv_img.data) << "Could not load " << lines_[lines_id_].first;
+//  cv::Mat cv_img = ReadImageToCVMat(root_folder + lines_[lines_id_].first,
+//      new_height, new_width, is_color);
+//  CHECK(cv_img.data) << "Could not load " << lines_[lines_id_].first;
+
+  cv::Mat cv_img_origin = ReadImageToCVMat(root_folder + lines_[lines_id_].first, is_color);
+  CHECK(cv_img_origin.data) << "Could not load " << lines_[lines_id_].first;
+
+  if (left_idx >= 0 && top_idx >= 0 && right_idx >= 0 && bottom_idx >= 0) {
+    const int img_width = cv_img_origin.cols;
+    const int img_height = cv_img_origin.rows;
+    const int patch_size = get_patch_size(left_idx, top_idx, right_idx, bottom_idx, img_width, img_height);
+    cv::Rect roi(left_idx, top_idx, patch_size, patch_size);
+    cv_img_origin = cv_img_origin(roi);
+  }
+
+  cv::Mat cv_img;
+  cv::resize(cv_img_origin, cv_img, cv::Size(new_width, new_height));
+  CHECK(cv_img.data) << "Could not get patch from " << lines_[lines_id_].first;
+
   // Use data_transformer to infer the expected blob shape from a cv_img.
   vector<int> top_shape = this->data_transformer_->InferBlobShape(cv_img);
   this->transformed_data_.Reshape(top_shape);
@@ -167,9 +229,26 @@ void ImageDataLayer<Dtype>::load_batch(Batch<Dtype>* batch) {
     // get a blob
     timer.Start();
     CHECK_GT(lines_size, lines_id_);
-    cv::Mat cv_img = ReadImageToCVMat(root_folder + lines_[lines_id_].first,
-        new_height, new_width, is_color);
-    CHECK(cv_img.data) << "Could not load " << lines_[lines_id_].first;
+
+//    cv::Mat cv_img = ReadImageToCVMat(root_folder + lines_[lines_id_].first,
+//        new_height, new_width, is_color);
+//    CHECK(cv_img.data) << "Could not load " << lines_[lines_id_].first;
+
+    cv::Mat cv_img_origin = ReadImageToCVMat(root_folder + lines_[lines_id_].first, is_color);
+    CHECK(cv_img_origin.data) << "Could not load " << lines_[lines_id_].first;
+
+    if (left_idx >= 0 && top_idx >= 0 && right_idx >= 0 && bottom_idx >= 0) {
+      const int img_width = cv_img_origin.cols;
+      const int img_height = cv_img_origin.rows;
+      const int patch_size = get_patch_size(left_idx, top_idx, right_idx, bottom_idx, img_width, img_height);
+      cv::Rect roi(left_idx, top_idx, patch_size, patch_size);
+      cv_img_origin = cv_img_origin(roi);
+    }
+
+    cv::Mat cv_img;
+    cv::resize(cv_img_origin, cv_img, cv::Size(new_width, new_height));
+    CHECK(cv_img.data) << "Could not get patch from " << lines_[lines_id_].first;
+
     read_time += timer.MicroSeconds();
     timer.Start();
     // Apply transformations (mirror, crop...) to the image
